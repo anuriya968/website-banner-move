f51467857fd79895418348517cb14ea2b60a7e64
Riya Dutta <duttariya2002@gmail.com>
2025-07-28 15:42:03 +0530

    Update HTML, CSS and JS files

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const slides = document.querySelectorAll('.slide');
-const slidesContainer = document.querySelector('.slides');
+const sliderContainer = document.querySelector('.slides');
 
 // Set the total width of the slides container
-slidesContainer.style.width = `${slides.length * 100}%`;
+sliderContainer.style.width = `${slides.length * 100}%`;
 
 let index = 0;
 
 function showSlide() {
   index++;
   if (index >= slides.length) index = 0;
-  slidesContainer.style.transform = `translateX(-${index * 100}%)`;
+  sliderContainer.style.transform = `translateX(-${index * 100}%)`;
 }
 
 setInterval(showSlide, 3000);
+
